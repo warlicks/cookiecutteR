2431af5464d3afd080d8fcc962ee30a149675809
Sean Warlick <warlick.sean@gmail.com>
2020-05-25 15:23:55 -0700

    removed old test

--- tests/testthat/test_directory_create.R
+++ tests/testthat/test_directory_create.R
@@ -14,6 +14,7 @@ test_dir <- file.path("..", "test_files", "test_project")
 if (dir.exists(test_dir)) {
     unlink(test_dir, recursive = TRUE, force = TRUE)
     }
+
 cookiecutteR:::create_file_structure(test_dir)
 
 # Run the Test
@@ -21,10 +22,10 @@ test_that('Check that file directory matches expected directory',
     expect_setequal(list.dirs(test_dir, full.names = FALSE), expected_structure)
 )
 
-test_that("An error occurs if we try to create the directory again",
-    expect_error(cookiecutteR:::create_file_structure(test_dir)
-                 )
-    )
+# test_that("An error occurs if we try to create the directory again",
+#     expect_error(cookiecutteR:::create_file_structure(test_dir)
+#                  )
+#     )
 
 # Clean Up
 unlink(test_dir, recursive = TRUE, force = TRUE)
