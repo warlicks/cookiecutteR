d2e041657f79f1242a24d48241b8b8e84a72846f
Sean Warlick <warlick.sean@gmail.com>
2020-06-19 22:03:18 -0700

    Updated tests to work with new architechture.

--- tests/testthat/test_other_file_create.R
+++ tests/testthat/test_other_file_create.R
@@ -1,12 +1,14 @@
 context("Test Other Files Created")
 
-test_directory <- file.path("..", 'test_files', 'other_files')
+test_directory <- fs::path_expand(file.path("~", 'other_files'))
 if (dir.exists(test_directory)) {
     unlink(test_directory, recursive = TRUE)
 }
-dir.create(test_directory, recursive = TRUE)
+usethis::proj_set(test_directory, force = TRUE)
+cookiecutteR:::create_file_structure(test_directory, FALSE)
 
 test_that('Check MakeFile Exists',{
+
           cookiecutteR:::create_makefile(test_directory)
           expect_true("Makefile" %in% list.files(test_directory))
 })
